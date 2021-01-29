8a353a6f8764816cc7a7f3f67407d2cad8c5b570
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-01-29 17:45:32 +0100

    added code

---
+++ challenges/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+
+# Create your views here.
+
+
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
+
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Eat no meat for the entire month!"
+    elif month == "february":
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif month == "march":
+        challenge_text = "Learn Django for at least 20 minutes every day!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
