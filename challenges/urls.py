5dccb1ce53cf4f48ceeff4e693c6af80a3771f3f
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-01-29 17:37:24 +0100

    cleanup

--- challenges/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index), # /challenges/
-    path("<int:month>", views.monthly_challenge_by_number),
-    path("<str:month>", views.monthly_challenge, name="month-challenge")
-]
