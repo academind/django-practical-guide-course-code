5d0a0d35f9e82c60bed7884676d5db391d96afec
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-01-29 17:59:35 +0100

    fixed urlpatterns error

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlspattern = [
+urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.post_detail,
