8b3bad4e6f4e6c7b6dff7e6b2d40e808abbec963
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-01-29 18:25:55 +0100

    added files

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
+from django.db.models import Avg
 
 from .models import Book
 
 # Create your views here.
 
 def index(request):
-  books = Book.objects.all()
+  books = Book.objects.all().order_by("-rating")
+  num_books = books.count()
+  avg_rating = books.aggregate(Avg("rating")) # rating__avg, rating__min
+
   return render(request, "book_outlet/index.html", {
-    "books": books
+    "books": books,
+    "total_number_of_books": num_books,
+    "average_rating": avg_rating
   })
 
 
