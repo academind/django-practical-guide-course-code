be24683d06d410332fc937118d89e53d15470efe
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-01-29 23:48:12 +0100

    added files

---
+++ reviews/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class ReviewForm(forms.Form):
+    user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
+        "required": "Your name must not be empty!",
+        "max_length": "Please enter a shorter name!"
+    })
+    review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
+    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
