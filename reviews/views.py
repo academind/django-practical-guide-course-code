2bbe0b5bb1eab4ee7d9dc21f0e9fa1be7ba66b0f
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-01-30 14:19:16 +0100

    added files

---
+++ reviews/views.py
@@ -0,0 +1,44 @@
+from reviews.models import Review
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+
+from .forms import ReviewForm
+from .models import Review
+
+# Create your views here.
+
+
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
+
+
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+
+
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=4)
+    #     return data
+
+
+class SingleReviewView(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
