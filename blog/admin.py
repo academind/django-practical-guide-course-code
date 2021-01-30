5d174f20f48f883068e9f7fe50bdc19ae40d2cc7
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-01-30 14:34:08 +0100

    added files

---
+++ blog/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from .models import Post, Author, Tag, Comment
+
+# Register your models here
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
