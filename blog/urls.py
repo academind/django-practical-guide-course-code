13fc393122a799d0806b867af55e640b30ab73d2
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-01-30 14:28:38 +0100

    added files

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("posts", views.AllPostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(),
+         name="post-detail-page")  # /posts/my-first-post
+]
