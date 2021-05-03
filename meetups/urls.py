91d6abe4ea09effd2632da5047c87b51abeac8b8
Maximilian Schwarzmüller <maximilian.schwarzmueller@academind.com>
2021-05-03 13:35:30 +0200

    added code

---
+++ meetups/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+  path('meetups', views.index, name='all-meetups'), # our-domain.com/meetups
+  path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail'), # our-domain.com/meetups/<dynamic-path-segment>
+]
